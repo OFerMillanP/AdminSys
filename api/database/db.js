cf034e6d3ab6307a378606854fba365a7ac8dc53
OFerMillanP <darkrayshadow28@gmail.com>
2026-07-12 21:44:34 -0600

    DataBase Connect Without Data

--- api/database/db.js
+++ api/database/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'adminSys',
-  password: process.env.DB_PASSWORD || 'DarkDemon8',
-  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || '',
+  password: process.env.DB_PASSWORD || '',
+  port: process.env.DB_PORT || '',
 });
 
 module.exports = pool;
